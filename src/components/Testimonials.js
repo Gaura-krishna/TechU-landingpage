8a2fe951ec82218c06ac9290efc67270706bfe39
Gaura krishna <gaurakrishna31@gmail.com>
2023-08-16 15:27:28 +0530

    My carousel removed

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -15,7 +15,7 @@ function Testimonials() {
 
                 <div>
                 <Carousel/>
-                {/* <MyCarousel/> */}
+                
                 </div>
 
             </div>
